1278052907613e68f22bfeb71ba04a42b15450ab
Jacob Sussmilch <suisuss@protonmail.com>
2026-03-29 16:29:26 +1100

    feat: CHAL-0008 add backend Zod validation schemas for notices module
    Add request validation for all notice and notice-recipient mutation
    endpoints: add/update notice, notice status, add/update/delete
    recipient. Field names match frontend schema expectations.

--- backend/src/modules/notices/notices-router.ts
+++ backend/src/modules/notices/notices-router.ts
@@ -1,20 +1,29 @@
 import { Router } from 'express';
 import * as noticeController from './notices-controller';
 import { checkApiAccess } from '../../middlewares';
+import { validateRequest } from '../../utils';
+import {
+  addNoticeSchema,
+  updateNoticeSchema,
+  noticeIdParamSchema,
+  noticeStatusSchema,
+  addNoticeRecipientSchema,
+  updateNoticeRecipientSchema
+} from './notices-schema';
 
 const router = Router();
 
 router.get('/recipients/list', checkApiAccess, noticeController.handleFetchNoticeRecipients);
 router.get('/recipients', checkApiAccess, noticeController.handleGetNoticeRecipients);
-router.get('/recipients/:id', checkApiAccess, noticeController.handleGetNoticeRecipient);
-router.post('/recipients', checkApiAccess, noticeController.handleAddNoticeRecipient);
-router.put('/recipients/:id', checkApiAccess, noticeController.handleUpdateNoticeRecipient);
-router.delete('/recipients/:id', checkApiAccess, noticeController.handleDeleteNoticeRecipient);
-router.post('/:id/status', checkApiAccess, noticeController.handleNoticeStatus);
+router.get('/recipients/:id', checkApiAccess, validateRequest(noticeIdParamSchema), noticeController.handleGetNoticeRecipient);
+router.post('/recipients', checkApiAccess, validateRequest(addNoticeRecipientSchema), noticeController.handleAddNoticeRecipient);
+router.put('/recipients/:id', checkApiAccess, validateRequest(updateNoticeRecipientSchema), noticeController.handleUpdateNoticeRecipient);
+router.delete('/recipients/:id', checkApiAccess, validateRequest(noticeIdParamSchema), noticeController.handleDeleteNoticeRecipient);
+router.post('/:id/status', checkApiAccess, validateRequest(noticeStatusSchema), noticeController.handleNoticeStatus);
 router.get('/pending', checkApiAccess, noticeController.handleFetchAllPendingNotices);
-router.get('/:id', checkApiAccess, noticeController.handleFetchNoticeDetailById);
+router.get('/:id', checkApiAccess, validateRequest(noticeIdParamSchema), noticeController.handleFetchNoticeDetailById);
 router.get('', checkApiAccess, noticeController.handleFetchAllNotices);
-router.post('', checkApiAccess, noticeController.handleAddNotice);
-router.put('/:id', checkApiAccess, noticeController.handleUpdateNotice);
+router.post('', checkApiAccess, validateRequest(addNoticeSchema), noticeController.handleAddNotice);
+router.put('/:id', checkApiAccess, validateRequest(updateNoticeSchema), noticeController.handleUpdateNotice);
 
 export { router as noticesRoutes };
