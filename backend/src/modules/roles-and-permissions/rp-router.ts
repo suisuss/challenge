e1c02a5467e916cfbcdcab927f78c0935e6afd3f
Jacob Sussmilch <suisuss@protonmail.com>
2026-03-29 16:39:18 +1100

    feat: CHAL-0008 add backend Zod validation schemas for roles-and-permissions module

--- backend/src/modules/roles-and-permissions/rp-router.ts
+++ backend/src/modules/roles-and-permissions/rp-router.ts
@@ -1,16 +1,25 @@
 import { Router } from 'express';
 import * as rpController from './rp-controller';
+import { validateRequest } from '../../utils';
+import {
+    roleIdParamSchema,
+    addRoleSchema,
+    updateRoleSchema,
+    roleStatusSchema,
+    rolePermissionsSchema,
+    switchRoleSchema,
+} from './rp-schema';
 
 const router = Router();
 
 router.get('', rpController.handleGetRoles);
-router.post('', rpController.handleAddRole);
-router.post('/switch', rpController.handleSwitchRole);
-router.put('/:id', rpController.handleUpdateRole);
-router.post('/:id/status', rpController.handleRoleStatus);
-router.get('/:id', rpController.handleGetRole);
-router.get('/:id/permissions', rpController.handleGetRolePermission);
-router.post('/:id/permissions', rpController.handleAddRolePermission);
-router.get('/:id/users', rpController.handleGetUsersByRoleId);
+router.post('', validateRequest(addRoleSchema), rpController.handleAddRole);
+router.post('/switch', validateRequest(switchRoleSchema), rpController.handleSwitchRole);
+router.put('/:id', validateRequest(updateRoleSchema), rpController.handleUpdateRole);
+router.post('/:id/status', validateRequest(roleStatusSchema), rpController.handleRoleStatus);
+router.get('/:id', validateRequest(roleIdParamSchema), rpController.handleGetRole);
+router.get('/:id/permissions', validateRequest(roleIdParamSchema), rpController.handleGetRolePermission);
+router.post('/:id/permissions', validateRequest(rolePermissionsSchema), rpController.handleAddRolePermission);
+router.get('/:id/users', validateRequest(roleIdParamSchema), rpController.handleGetUsersByRoleId);
 
 export { router as rpRoutes };
