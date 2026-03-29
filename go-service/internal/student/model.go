978eaddc6a39b603ece07e6ed4db570f3292a30c
Jacob Sussmilch <suisuss@protonmail.com>
2026-03-29 15:59:22 +1100

    style: CHAL-0004 gofmt student model.go

--- go-service/internal/student/model.go
+++ go-service/internal/student/model.go
@@ -3,25 +3,25 @@ package student
 import "time"
 
 type Student struct {
-	ID                  int
-	Name                string
-	Email               string
-	SystemAccess        bool
-	Phone               *string
-	Gender              *string
-	DOB                 *time.Time
-	Class               *string
-	Section             *string
-	Roll                *int
-	FatherName          *string
-	FatherPhone         *string
-	MotherName          *string
-	MotherPhone         *string
-	GuardianName        *string
-	GuardianPhone       *string
-	RelationOfGuardian  *string
-	CurrentAddress      *string
-	PermanentAddress    *string
-	AdmissionDate       *time.Time
-	ReporterName        *string
+	ID                 int
+	Name               string
+	Email              string
+	SystemAccess       bool
+	Phone              *string
+	Gender             *string
+	DOB                *time.Time
+	Class              *string
+	Section            *string
+	Roll               *int
+	FatherName         *string
+	FatherPhone        *string
+	MotherName         *string
+	MotherPhone        *string
+	GuardianName       *string
+	GuardianPhone      *string
+	RelationOfGuardian *string
+	CurrentAddress     *string
+	PermanentAddress   *string
+	AdmissionDate      *time.Time
+	ReporterName       *string
 }
