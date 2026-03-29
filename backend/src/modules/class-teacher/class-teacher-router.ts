408d7ac72b084539a34108c042580c2435fb23c1
Jacob Sussmilch <suisuss@protonmail.com>
2026-03-29 16:39:18 +1100

    feat: CHAL-0008 add backend Zod validation schemas for class-teacher module

--- backend/src/modules/class-teacher/class-teacher-router.ts
+++ backend/src/modules/class-teacher/class-teacher-router.ts
@@ -1,12 +1,14 @@
 import { Router } from 'express';
 import * as classTeacherController from './class-teacher-controller';
 import { checkApiAccess } from '../../middlewares';
+import { validateRequest } from '../../utils';
+import { classTeacherIdParamSchema, addClassTeacherSchema, updateClassTeacherSchema } from './class-teacher-schema';
 
 const router = Router();
 
 router.get('', checkApiAccess, classTeacherController.handleGetClassTeachers);
-router.post('', checkApiAccess, classTeacherController.handleAddClassTeacher);
-router.get('/:id', checkApiAccess, classTeacherController.handleGetClassTeacherDetail);
-router.put('/:id', checkApiAccess, classTeacherController.handleUpdateClassTeacherDetail);
+router.post('', checkApiAccess, validateRequest(addClassTeacherSchema), classTeacherController.handleAddClassTeacher);
+router.get('/:id', checkApiAccess, validateRequest(classTeacherIdParamSchema), classTeacherController.handleGetClassTeacherDetail);
+router.put('/:id', checkApiAccess, validateRequest(updateClassTeacherSchema), classTeacherController.handleUpdateClassTeacherDetail);
 
 export { router as classTeacherRoutes };
